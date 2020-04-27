b25825a8d9350092001e47fd6935d1a0a8965005
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-27 18:37:59 -0400

    Added textFields programmatically. User can now dynamically add a new textField step.

--- How-To/How-To/View Controllers/CreateHowToViewController.swift
+++ How-To/How-To/View Controllers/CreateHowToViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class CreateHowToViewController: UIViewController {
-
-    //TODO: Programmatically setup adding step textfields
     
     // MARK: - IBOutlets
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var summaryTextView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var categoryView: UIView!
+    @IBOutlet weak var detailStackView: UIStackView!
+    @IBOutlet weak var contentView: UIView!
+    
+    // MARK: - Properties
+    var textFields: [UITextField] = []
+    var textFieldsStack = UIStackView()
+    var numberOfSteps = 1
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
@@ -30,12 +35,45 @@ class CreateHowToViewController: UIViewController {
         summaryTextView.layer.cornerRadius = 8
         categoryView.layer.cornerRadius = 8
         
+        textFieldsStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textFieldsStack)
+        textFieldsStack.alignment = .fill
+        textFieldsStack.axis = .vertical
+        textFieldsStack.distribution = .fill
+        textFieldsStack.spacing = 8
         
+        let stackLeadingConstraint = textFieldsStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+        let stackBottomConstraint = textFieldsStack.topAnchor.constraint(equalTo: detailStackView.bottomAnchor, constant: 8)
+        let stackTrailingConstraint = textFieldsStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
         
+        let addStepButton = UIButton(type: .system)
+        addStepButton.setTitle("+ Add Step", for: .normal)
+        addStepButton.addTarget(self, action: #selector(addTextField), for: .touchUpInside)
+        addStepButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(addStepButton)
         
+        let buttonTopConstraint = addStepButton.topAnchor.constraint(equalTo: textFieldsStack.bottomAnchor, constant: 8)
+        let buttonLeadingConstraint = addStepButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+        let buttonTrailingConstraint = addStepButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        let buttonBottomConstraint = addStepButton.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: 2)
+        let buttonHeightConstraint = addStepButton.heightAnchor.constraint(equalToConstant: 50)
         
+        NSLayoutConstraint.activate([stackLeadingConstraint, stackBottomConstraint, stackTrailingConstraint, buttonTopConstraint, buttonLeadingConstraint, buttonTrailingConstraint, buttonBottomConstraint, buttonHeightConstraint])
         
-        
+        addTextField()
+    }
+    
+    @objc private func addTextField() {
+        let textField = UITextField()
+        textField.placeholder = "  Step \(numberOfSteps)"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        view.addSubview(textField)
+        textFields.append(textField)
+        textField.backgroundColor = UIColor(named: "Text Field Background Color")
+        textField.layer.cornerRadius = 8
+        textFieldsStack.addArrangedSubview(textField)
+        numberOfSteps += 1
     }
 
     // MARK: - IBActions
