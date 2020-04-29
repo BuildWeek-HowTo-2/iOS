218bb1a3847204487cdc6495abc6985ea6d5048f
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-29 11:44:07 -0400

    Made changes to CoreData

--- How-To/Model/Tutorial.swift
+++ How-To/Model/Tutorial.swift
@@ -10,14 +10,14 @@ import Foundation
 
 struct Tutorial: Codable {
     let id: Int
-    let title: String
-    let summary: String
+    var title: String
+    var summary: String
     var tutorialSteps: [TutorialSteps]?
-    let likes: Int
+    var likes: Int
     let instructor_id: Int
 }
 
 struct TutorialSteps: Codable {
-    let instructions: String
+    var instructions: String
     let step_number: Int
 }
